8af454a1df3c2d152aa80d80cc8536d0caae530e
aimalm <aimal.maarij2020@gmail.com>
2020-11-10 22:24:48 +0100

    completing 02-calculator-two exercise

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -9,13 +9,38 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
 (() => {
+    
+    var value1;
+    var value2;
     // to get the value of an input: document.getElementById("element-id").value
+    document.getElementById("op-one").addEventListener("change", (event) => {
+        value1 =  parseInt(document.getElementById("op-one").value);
+    });
+
+    document.getElementById("op-two").addEventListener("change", (event) => {
+        value2 =  parseInt(document.getElementById("op-two").value);
+    });
 
     const performOperation = operation => {
         // perform the operation
+        switch(operation) {
+            case 'addition':
+                alert(value1 + value2);
+                break;
+            case 'substraction':
+                alert(value1 - value2);
+                break;
+            case 'multiplication':
+                alert(value1 * value2);
+                break;
+            case 'division':
+                alert(value1 / value2);
+                break;  
+        }
     };
-
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
         $btn.addEventListener(
             "click",
