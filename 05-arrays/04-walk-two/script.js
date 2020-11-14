914731b16625508f91743057bcc0ff3691d65ce7
aimalm <aimal.maarij2020@gmail.com>
2020-11-14 12:38:28 +0100

    completting JS array exercise 04-walk-two

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -27,5 +27,11 @@
     ];
 
     // your code here
+    document.getElementById("run").addEventListener("click", function(){
+        fruits.forEach(function(item, index, array){
+            console.log(item);
+
+        })
+    })
 
 })();
