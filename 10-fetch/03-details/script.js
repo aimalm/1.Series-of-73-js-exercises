238ccd71bf56c764ef5a23a1be22bd298e4021f0
aimalm <aimal.maarij2020@gmail.com>
2020-12-01 01:06:55 +0100

    exercise 03-details of 10-fetch JS

--- 10-fetch/03-details/script.js
+++ 10-fetch/03-details/script.js
@@ -11,4 +11,46 @@
 
 (() => {
     // your code here
+    const tem = document.querySelector("#tpl-hero")
+    let targetElement = document.getElementById('target');
+    const btn = document.getElementById("run")
+    let inputID = document.getElementById("hero-id")
+
+    btn.addEventListener("click", () =>{
+    let id = inputID.value
+
+    fetch('http://localhost:3000/heroes')
+    .then(
+      function(response) {
+        //console.log(response)
+
+      
+        response.json().then(function (heroes) {
+
+           
+            for(var i = 0; i < heroes.length; i++) {
+                if (heroes[i].id == id) {
+                    
+                    //console.log(heroes[i].id)
+                    var heroElement = tem.content.cloneNode(true);
+                    heroElement.querySelector('.name').innerHTML = heroes[i].name;
+                    heroElement.querySelector('.alter-ego').innerHTML = heroes[i].alterEgo;
+                    heroElement.querySelector('.powers').innerHTML = heroes[i].abilities
+                    targetElement.appendChild(heroElement);
+                    break
+
+                } /* else {
+                    let invalidMsg = document.createElement("h2");
+                    invalidMsg.innerHTML = "Invalid"
+                    targetElement.appendChild(invalidMsg);
+                } */
+                
+
+             };
+             
+        });
+      }
+    );
+});
+
 })();
